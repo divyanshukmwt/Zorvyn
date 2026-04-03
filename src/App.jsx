c44235d777ebaf77fd293bc8d51711300f233af7
divyanshukmwt <officialdivyanshu18@gmail.com>
2026-04-03 12:00:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+
+  return (
+    <h1 className="text-red-500 text-3xl">
+  Tailwind Working 🚀
+</h1>
+  )
+}
+
+export default App
